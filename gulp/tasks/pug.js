fbece114652d577f7301fe4fd071a629797803f8
sid <sidmorris@mail.ru>
2018-03-10 08:55:36 +0300

    end home service

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,9 +1,13 @@
 'use strict';
+var fs = require('fs');
 
 module.exports = function() {
   $.gulp.task('pug', function() {
     return $.gulp.src('./source/template/pages/*.pug')
-      .pipe($.gp.pug({ pretty: true }))
+      .pipe($.gp.pug({
+         pretty: true,
+         locals: JSON.parse(fs.readFileSync('./source/template/data.json', 'utf8')) 
+        }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
           title: 'Pug',
--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,23 @@
-(function() {
+(function () {
   'use strict';
-
+  $('.carousel-services').owlCarousel({
+    loop: true,
+    nav: true,
+    smartSpeed: 700,
+    navText: ['<svg class="icon icon-chevron-left"><use xlink:href="assets/img/sprite.svg#chevron-left"></use></svg>',
+      '<svg class="icon icon-chevron-right"><use xlink:href="assets/img/sprite.svg#chevron-right"></use></svg>'],
+      responsiveClass: true,
+      responsive:{
+        0:{
+          items :  1
+        },
+        800:{
+          items:2
+        },
+        1100:{
+          items:3
+        }
+      }
+  });
 
 })();
