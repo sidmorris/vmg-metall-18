3a3d33d50255c2231fa14d79f7d0ec0d8b78d40e
sidmorris <sidmorris@mail.ru>
2018-03-11 22:41:50 +0300

    end totop edit modal

--- source/js/app.js
+++ source/js/app.js
@@ -20,16 +20,65 @@
   //     }
   // });
   $('.fotorama').fotorama({
-  width: 900,
-  minwidth:300,
-  maxwidth: '100%',
-  arrows: 'always',
-  loop: true,
-  allowfullscreen: false,
-  thumbborderwidth:3,
-  thumbmargin:6,
-  thumbwidth:120,
-  nav: 'thumbs'
-});
+    width: 900,
+    minwidth: 300,
+    maxwidth: '100%',
+    arrows: 'always',
+    loop: true,
+    allowfullscreen: false,
+    thumbborderwidth: 3,
+    thumbmargin: 6,
+    thumbwidth: 120,
+    nav: 'thumbs'
+  });
+
+  // Smoosh Scroll
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
+// Scroll To Top
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("toTop").style.display = "block";
+    } else {
+        document.getElementById("toTop").style.display = "none";
+    }
+}
 
 })();
